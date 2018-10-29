03171a3d3ffce571c197fe4b195dd84d214d482a
John Reese <john@noswap.com>
2018-10-28 22:15:30 -0700

    Apply isort with appropriate arguments before running black

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md") as f:
     readme = f.read()
