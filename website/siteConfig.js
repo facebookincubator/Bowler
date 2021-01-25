c89e23ffb9742028838708fd0f28b0bf89adc153
John Reese <john@noswap.com>
2021-01-25 12:45:44 -0800

    Add cname config for future builds

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,6 +26,7 @@ const siteConfig = {
   title: 'Bowler' /* title for your website */,
   tagline: 'Safe code refactoring for modern Python',
   url: 'https://pybowler.io' /* your website url */,
+  cname: 'pybowler.io' /* custom gh-pages website domain */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
